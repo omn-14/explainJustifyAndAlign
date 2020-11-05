c98ca22f5e60b22d522a40a0e9f8804172283fa6
Otavio Miklos <otaviomiklos@gamil.com>
2020-11-05 08:54:51 -0300

    Being more semantic, and deleting some useless code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body');
 const textJust = document.querySelector('.text-just');
 const textAlign = document.querySelector('.text-align');
 
+const pos = ['start', 'center', 'end'];
+
 var just = 0;
 var align = 0;
 
@@ -22,27 +24,17 @@ function mover(event) {
 }
 
 function verificar(value) {
-    if(value<0) value=0;
-    if(value>2) value=2;
-    return value;
+    if(value<0) return 0;
+    if(value>2) return 2;
 }
 
 function colocarClasses(just, align) {
-    for(let i = 0; i < 3; i++) {
-        body.classList = '';
-        
-        body.classList.add(`just-${just}`);
-        body.classList.add(`align-${align}`);
-    }
-    console.log(just, align);
+    body.classList = '';
+    body.classList.add(`just-${pos[just]}`);
+    body.classList.add(`align-${pos[align]}`);
 }
 
 function alterarTexto(just, align) {
-    const pos = ['start', 'center', 'end'];
-    for(let i = 0; i < 3; i++) {
-        if(just==i) justText = pos[i];
-        if(align==i) alignText = pos[i];
-    }
-    textJust.textContent = `justify-items: ${justText}`;
-    textAlign.textContent = `align-items: ${alignText}`;
+    textJust.textContent = `justify-items: ${pos[just]}`;
+    textAlign.textContent = `align-items: ${pos[align]}`;
 }
